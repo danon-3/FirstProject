b935f4f8a7d847a40bdcbb3e49a9e9c38c5e74f3
danilmaklov <dmyaklov@mail.ru>
2022-01-18 15:17:03 +0300

    Task 6.1

--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -9,12 +9,17 @@ public class Task6 {
         String[] numbers = scan.nextLine().split(" ");
         String[] num2 = new String[numbers.length];
         int m = 0;
-        for(int i = 0; i <= numbers.length; i++){
-            if(!numbers[i].equals(0)){
+        for(int i = 0; i < numbers.length; i++){
+            if(!numbers[i].equals("0")){
                 num2[m] = numbers[i];
                 m = m + 1;
             }
         }
+        for (int i = m; i < numbers.length; i++){
+            num2[m] = "0";
+            m = m + 1;
+        }
+
         System.out.println(Arrays.toString(num2));
     }
 }
--- src/com/company/Task8.java
+++ src/com/company/Task8.java
@@ -1,25 +1,16 @@
 package com.company;
-import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Task8 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[] numbers = scan.nextLine().split(" ");
-        String[] num2 = new String[numbers.length];
-        int m = 0;
-        for(int i = 0; i < numbers.length; i++){
-            if(!numbers[i].equals("0")){
-                num2[m] = numbers[i];
-                m = m + 1;
-            }
+        int n = Integer.parseInt(scan.nextLine());
+        int m = 1;
+        while(n != 0){
+            m = m * n;
+            n = n - 1;
         }
-        for (int i = m; i < numbers.length; i++){
-            num2[m] = "0";
-            m = m + 1;
-        }
-
-        System.out.println(Arrays.toString(num2));
+        System.out.println(m);
     }
 }
