9f057254db8abca5c4c58b83b9b48e0da0ad5940
danilmaklov <dmyaklov@mail.ru>
2022-01-18 15:15:09 +0300

    Task 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,27 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = Integer.parseInt(scan.nextLine());
-        int m = 1;
-        while(n != 0){
-            m = m * n;
-            n = n - 1;
+        String[] numbers = scan.nextLine().split(" ");
+        String[] num2 = new String[numbers.length];
+        int m = 0;
+        for(int i = 0; i < numbers.length; i++){
+            if(!numbers[i].equals("0")){
+                num2[m] = numbers[i];
+                m = m + 1;
+            }
         }
-        System.out.println(m);
+        for (int i = m; i < numbers.length; i++){
+            num2[m] = "0";
+            m = m + 1;
+        }
+
+        System.out.println(Arrays.toString(num2));
     }
     }
 
--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -1,25 +1,20 @@
 package com.company;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-
-public class Task8 {
+public class Task6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String[] numbers = scan.nextLine().split(" ");
         String[] num2 = new String[numbers.length];
         int m = 0;
-        for(int i = 0; i < numbers.length; i++){
-            if(!numbers[i].equals("0")){
+        for(int i = 0; i <= numbers.length; i++){
+            if(!numbers[i].equals(0)){
                 num2[m] = numbers[i];
                 m = m + 1;
             }
         }
-        for (int i = m; i < numbers.length; i++){
-            num2[m] = "0";
-            m = m + 1;
-        }
-
         System.out.println(Arrays.toString(num2));
     }
 }
