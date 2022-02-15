fce47f1a59216f59cb43148f928dd8de0fbcd737
danilmaklov <dmyaklov@mail.ru>
2022-02-15 13:18:44 +0300

    Task 6.1

---
+++ src/com/company/Arrays_15Feb.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Arrays_15Feb {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr1 = scanner.nextLine().split(" ");
+        int[] given = new int[arr1.length];
+        boolean decreasing = true;
+        boolean increasing = true;
+
+        for (int i=0; i < arr1.length; i++)
+            given[i] = Integer.valueOf(arr1[i]);
+
+
+        for (int i=0; i < given.length; i++){
+            if (given[i]<given[i+1]){
+                decreasing = false;
+                break;
+            }
+        }
+
+        for (int i=0; i < given.length; i++){
+            if (given[i]>given[i+1]){
+                increasing = false;
+                break;
+            }
+        }
+
+        if (increasing && decreasing)
+            System.out.println("All elements of the array are equal");
+        else if (!increasing && decreasing)
+            System.out.println("Array is sorted in decreasing order");
+        else if (increasing && !decreasing)
+            System.out.println("Array is sorted in increasing order");
+        else if (!increasing && !decreasing)
+            System.out.println("Array is not sorted");
+
+
+    }
+}
